cc666d7c9208ce9b04816e970c504a3f039e5859
tomas.lajda <tomas.lajda0@gmail.com>
2024-07-31 19:47:53 +0200

    updated connection so its not blocking way

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <Arduino.h>
+#include <WiFi.h>
 #include "config.h"
-#include <WiFiMulti.h>
-
-WiFiMulti wifiMulti;
 
 const int LedPin = 4;
 
@@ -10,16 +8,24 @@ void setup() {
   Serial.begin(921600);
   pinMode(LedPin, OUTPUT);
 
-  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  Serial.println("Connecting ...");
-  while(wifiMulti.run() != WL_CONNECTED) {
-    delay(100);
-  }
-  Serial.println("Connected to wifi");
-
+  Serial.println("Starting WiFi connection...");
 }
 
+bool isConnected = false;
+
 void loop() {
-  digitalWrite(LedPin, WiFi.status() == WL_CONNECTED);
+  if (WiFi.status() == WL_CONNECTED && !isConnected) {
+    Serial.println("Connected to WiFi!");
+    digitalWrite(LedPin, WiFi.status() == WL_CONNECTED);
+    isConnected = true;
+  }
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println(".");
+    digitalWrite(LedPin, !digitalRead(LedPin));
+    delay(1000);
+    isConnected = false;
+  }
 }
