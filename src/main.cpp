4f39e4811660a36f16480aa99614d4423c520093
tomas.lajda <tomas.lajda0@gmail.com>
2024-07-31 14:34:38 +0200

    Printing scrolling message

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,30 @@ int lcdRows = 2;
 
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
 
+String messageStatic = "Static message";
+String messageToScroll = "This is a scrolling message with more than 16 characters";
+
+void scrollMessage(int row, String message, int delayTime, int lcdColumns) {
+  for (int i = 0; i < lcdColumns; ++i) {
+    message = " " + message;  
+  } 
+  message = message + " "; 
+
+  for (int pos = 0; pos < message.length(); pos++) {
+    lcd.setCursor(0, row);
+    lcd.print(message.substring(pos, pos + lcdColumns));
+    delay(delayTime);
+  }
+}
+
 void setup(){
-  lcd.init();
+  lcd.init();                    
   lcd.backlight();
 }
 
 void loop(){
   lcd.setCursor(0, 0);
-  lcd.print("Hello, World!");
-  delay(1000);
-  lcd.clear();
-
-  lcd.setCursor(0,1);
-  lcd.print("Hello, World!");
-  delay(1000);
-  lcd.clear(); 
+  lcd.print(messageStatic);
+  
+  scrollMessage(1, messageToScroll, 250, lcdColumns);
 }
