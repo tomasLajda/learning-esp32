90d355dc0a317ad692d867ec91341971851d799e
tomas.lajda <tomas.lajda0@gmail.com>
2024-08-03 22:39:01 +0200

    added wifi and set gpio pins

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,26 @@
 #include <Arduino.h>
 #include <WiFi.h>
+
 #include "config.h"
 
 const int LedPin = 4;
+const int SDA = 21;
+const int SCL = 22;
 
 void setup() {
   Serial.begin(921600);
   pinMode(LedPin, OUTPUT);
 
+  Serial.println("Starting WiFi connection...");
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  Serial.println("Starting WiFi connection...");
-}
-
-bool isConnected = false;
-
-void loop() {
-  if (WiFi.status() == WL_CONNECTED && !isConnected) {
-    Serial.println("Connected to WiFi!");
-    digitalWrite(LedPin, WiFi.status() == WL_CONNECTED);
-    isConnected = true;
-  }
-
-  if (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.println(".");
-    digitalWrite(LedPin, !digitalRead(LedPin));
-    delay(1000);
-    isConnected = false;
+    delay(300);
   }
+
+  Serial.print("/nConnected With Ip: ");
+  Serial.println(WiFi.localIP());
 }
+
+void loop() { delay(1000); }
