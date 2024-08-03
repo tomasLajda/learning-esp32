5544ba918a46cad0cc1a6b041ab009bc9622e7e0
tomas.lajda <tomas.lajda0@gmail.com>
2024-08-03 22:39:39 +0200

    motion displaying on lcd

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,25 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
 #include <WiFi.h>
 
 #include "config.h"
 
-const int LedPin = 4;
-const int SDA = 21;
-const int SCL = 22;
+const byte LedPin = 4;
+const byte MotionPin = 5;
+
+const byte LcdAddress = 0x27;
+const byte LcdRows = 2;
+const byte LcdColumns = 16;
+
+LiquidCrystal_I2C lcd(LcdAddress, LcdColumns, LcdRows);
 
 void setup() {
-  Serial.begin(921600);
+  Serial.begin(115200);
   pinMode(LedPin, OUTPUT);
+  pinMode(MotionPin, INPUT);
+
+  lcd.init();
+  lcd.backlight();
 
   Serial.println("Starting WiFi connection...");
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -19,8 +29,23 @@ void setup() {
     delay(300);
   }
 
-  Serial.print("/nConnected With Ip: ");
+  Serial.print("Connected With Ip: ");
   Serial.println(WiFi.localIP());
 }
 
-void loop() { delay(1000); }
+void loop() {
+  lcd.setCursor(0, 0);
+  lcd.clear();
+
+  if (digitalRead(MotionPin) == HIGH) {
+    lcd.print("Motion detected");
+    digitalWrite(LedPin, HIGH);
+    Serial.println("High");
+  } else {
+    lcd.print("No motion.");
+    digitalWrite(LedPin, LOW);
+    Serial.println("LOW");
+  }
+
+  delay(500);
+}
